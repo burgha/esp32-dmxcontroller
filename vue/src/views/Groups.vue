9ec291705c8c9219a6aa23aa15d97350a9a8af4d
Hannes Burger <21040544+burgha@users.noreply.github.com>
2023-11-15 21:42:13 +0100

    [vue] cleanup

--- vue/src/views/Groups.vue
+++ vue/src/views/Groups.vue
@@ -7,18 +7,6 @@
                 <v-btn class="ma-2" @click="editGroup(group as Group)">Edit</v-btn>
                 <v-btn class="ma-2" @click="deleteGroup(group as Group)">Delete</v-btn>
             </div>
-            <!--
-            <v-container>
-                <p>Animations:</p>
-                <v-btn @click="addAnimation(group as Group)">Add</v-btn>
-                <v-row v-for="animation in store.animations" :key="animation.name" justify="center">
-                    <p>{{ animation.name }}</p><v-btn
-                        @click="editAnimation(animation as Animation, group as Group)">Edit</v-btn><v-btn
-                        @click="deleteAnimation(animation as Animation)">Delete</v-btn>
-                </v-row>
-                <v-divider v-if="store.groups.indexOf(group) < store.groups.length - 1" />
-            </v-container>
-            -->
         </div>
 
         <v-btn class="mt-4" @click="createGroup()">Create</v-btn>
@@ -43,32 +31,6 @@
             </v-card-actions>
         </v-card>
     </v-dialog>
-
-    <!--
-    <v-dialog v-model="animationDialog" width="80%">
-        <v-card>
-            <v-card-text>
-                <v-text-field v-model="animationDialogAnimation!.name" placeholder="Name"></v-text-field>
-                <div class="timeline" style="position: relative; display: flex; width: 100%">
-                    <div id="outline" style="height: 500px;">
-                        <div v-for="fixture in animationDialogGroup?.members">
-                            <div v-for="(n, channel) in fixture.numChannels">
-                                <p>{{ fixture.name }} / {{ channel + 1}} <v-btn @click="addKeyframe(fixture, channel)">Add Keyframe</v-btn></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="keyframes" style="height: 500px; flex-grow: 1;"></div>
-                </div>
-            </v-card-text>
-            <v-card-actions>
-                <v-btn v-if="animationDialogUpdating" color="primary" block
-                    @click="saveAnimation(); animationDialog = false">Speichern</v-btn>
-                <v-btn v-if="!animationDialogUpdating" color="primary" block
-                    @click="createAnimation(); animationDialog = false">Hinzufügen</v-btn>
-            </v-card-actions>
-        </v-card>
-    </v-dialog>
-    -->
 </template>
 
 <script setup lang="ts">
@@ -76,8 +38,6 @@ import Fixture from '@/models/Fixture';
 import Group from '@/models/Group';
 import { useDmxStore } from '@/stores/dmx';
 import { ref } from 'vue';
-// import Animation from '@/models/Animation';
-// import { Timeline } from 'animation-timeline-js';
 
 const store = useDmxStore();
 
@@ -86,12 +46,6 @@ let editingGroupMembers = ref([] as Fixture[]);
 let editing = ref(false);
 
 let groupEditDialog = ref(false);
-
-// let animationDialog = ref(false);
-// let animationDialogAnimation = ref<Animation | null>();
-// let animationDialogUpdating = ref(false);
-// let animationDialogGroup = ref<Group | null>();
-// let animationDialogTimeline: Timeline | null = null;
 
 function createGroup(): void {
     groupEditDialog.value = true;
@@ -141,63 +95,4 @@ function setEditingMembershipValue(v: boolean, f: Fixture) {
         editingGroupMembers.value.splice(editingGroupMembers.value.indexOf(f));
     }
 }
-
-// function addAnimation(group: Group) {
-//     animationDialogUpdating.value = false;
-//     animationDialog.value = true;
-//     animationDialogAnimation.value = new Animation("", group);
-//     animationDialogGroup.value = group;
-//     initTimeline(group);
-// }
-
-// function initTimeline(group: Group) {
-//     setTimeout(() => {
-//         const model: any = { rows: [] }
-//         group.members.forEach(x => {
-//             if (!animationDialogAnimation.value!.keyframes.has(x)) {
-//                 const map = new Map();
-//                 for (let i = 0; i < x.numChannels; i++) {
-//                     map.set(i, []);
-//                 }
-//                 animationDialogAnimation.value!.keyframes.set(x, map)
-//             }
-//         });
-//         for (let [fixture, map] of animationDialogAnimation.value!.keyframes) {
-//             for (let [number, keyframes] of map) {
-//                 model.rows.push(keyframes);
-//             }
-//         }
-//         animationDialogTimeline = new Timeline({ id: 'keyframes' });
-//         animationDialogTimeline.setModel(model);
-//     }, 500)
-// }
-
-// function addKeyframe(fixture: Fixture, channel: number) {
-//     const model = animationDialogTimeline?.getModel();
-//     model?.rows[channel].keyframes?.push({val: animationDialogTimeline!.getTime(), value: 0});
-//     animationDialogTimeline?.setModel(model);
-// }
-
-// function editAnimation(animation: Animation, group: Group) {
-//     animationDialogUpdating.value = true;
-//     animationDialogAnimation.value = animation;
-//     animationDialog.value = true;
-//     animationDialogGroup.value = group;
-//     initTimeline(group);
-// }
-
-// function deleteAnimation(animation: Animation) {
-//     const index = store.animations.indexOf(animation);
-//     store.animations.splice(index, 1);
-//     store.persistState();
-// }
-
-// function createAnimation() {
-//     store.animations.push(animationDialogAnimation.value!);
-//     store.persistState();
-// }
-
-// function saveAnimation() {
-//     store.persistState();
-// }
 </script>
